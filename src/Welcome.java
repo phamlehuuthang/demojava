31b7a4f2c52fee45f0b5d7440c6325dd6d984e36
phamlehuuthang <23654731.thang@student.iuh.edu.vn>
2024-08-12 17:57:08 +0700

    demo project

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args){
+        System.out.println("Hello Pham Le Huu Thang");
+        System.out.println("Welcom to the DHKTPM19ATT");
+    }
+}
